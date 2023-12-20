1c74b3ea555ce961849a9b8c99a71c3bbd1f03d9
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-12-20 16:18:04 +0800

    Add production environment configuration and plugins

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel', 'module:react-native-dotenv'],
+      },
+    },
+    presets: ['module:metro-react-native-babel-preset', 'babel-preset-expo'],
+    plugins: [["module:react-native-dotenv"]],
   };
 };
+
