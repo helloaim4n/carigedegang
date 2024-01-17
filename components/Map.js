998d6196bf44aba4059f420bc5b1a1c3352bd70e
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-01-18 02:30:23 +0800

    Update Map component with new features and styling

--- components/Map.js
+++ components/Map.js
@@ -1,30 +1,48 @@
-
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Dimensions, Text } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE, Callout, Circle } from 'react-native-maps';
 
 const Map = () => {
+  const region = {
+    latitude: 37.7749,
+    longitude: -122.4194,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+
   return (
     <MapView
       provider={PROVIDER_GOOGLE}
       style={styles.map}
-      region={{
-        latitude: 37.7749,
-        longitude: -122.4194,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      // onRegionChangeComplete={region => {
-      //   console.log("Region changed to:", region);
-      // }}
+      region={region}
       showsUserLocation={true}
       followsUserLocation={true}
-      zoomControlEnabled={true}
+      userLocationAnnotationTitle='My Location'
+      showsTraffic={true}
+      showsMyLocationButton={true}
+      showsCompass={true}
+      showsScale={true}
+      minZoomLevel={10}  // minimum zoom (more zoomed out)
+      maxZoomLevel={20} // maximum zoom (more zoomed in)
     >
       <Marker 
-        coordinate={{latitude: 37.7749, longitude: -122.4194}}
-        title={"Marker Title"}
-        description={"Marker Description"}
+        coordinate={region}
+        title={"San Francisco"}
+        description={"A vibrant city in California"}>
+        <Callout>
+          <React.Fragment>
+            <Text style={styles.calloutTitle}>San Francisco</Text>
+            <Text>A vibrant city in California.</Text>
+          </React.Fragment>
+        </Callout>
+      </Marker>
+
+      <Circle 
+        center={region}
+        radius={1200} 
+        fillColor={"rgba(135, 206, 250, 0.5)"}
+        strokeColor={"rgba(135, 206, 250, 1)"}
+        strokeWidth={2}
       />
     </MapView>
   );
@@ -32,13 +50,12 @@ const Map = () => {
 
 const styles = StyleSheet.create({
   map: {
-    flex: 1,
-    borderWidth: 15,
-    borderColor: '#000',
-    borderRadius: 10,
-    margin: 25,
-    marginBottom: 15,
-    marginTop: 0,
+    height: Dimensions.get('window').height / 2, // Half the height of the screen
+    width: '100%',
+  },
+  calloutTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
 
